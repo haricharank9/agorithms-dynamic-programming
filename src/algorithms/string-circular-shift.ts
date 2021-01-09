ea18cae103dbbbab926a4c6917a4a0f931c14e9d
HARI CHARAN VARMA KOKKU <haricharank9@gmail.com>
2021-01-09 15:10:06 -0600

    Update string-circular-shift.ts

--- src/algorithms/string-circular-shift.ts
+++ src/algorithms/string-circular-shift.ts
@@ -9,3 +9,26 @@ export const circularShift = (str: string, left: number, right: number) => {
   }
   return shiftedStr;
 };
+
+/** 
+ * Implementation without helper function
+ * @example
+ export const circularShift = (str: string, left: number, right: number) => {
+  let shiftedStr: string = str;
+  const shiftLeft = (str: string, shift: number) =>
+    str.substr(shift) + str.substr(0, shift);
+
+  const shiftRight = (str: string, shift: number) =>
+    str.substr(str.length - shift) + str.substr(0, str.length - shift);
+  const absoluteShift = (left - right) % str.length;
+  if (absoluteShift) {
+    shiftedStr =
+      absoluteShift < 0
+        ? shiftRight(str, Math.abs(absoluteShift))
+        : shiftLeft(str, Math.abs(absoluteShift));
+    }
+  return shiftedStr;
+  };
+ *
+ *
+*/
